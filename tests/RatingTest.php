a3ef3fa8ca0493f4cb5cb4b8a1a2069f1ea7ddd3
Enas <enasasmar882@gmail.com>
2022-10-19 19:11:12 +0300

    tests

---
+++ tests/RatingTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Corals\UtilityRating\Tests;
+
+//use PHPUnit\Framework\TestCase;
+class RatingTest extends TestCase
+{
+    public function test_ratings()
+    {
+        $response = $this->json('utilities/ratings');
+        $response->assertStatus(200);
+        //$this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Corals\UtilityRating\Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+
+}
